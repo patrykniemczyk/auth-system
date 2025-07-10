0714469693007816af95773719564398c5c36052
Patryk Niemczyk <patryk.niemczyk12345@gmail.com>
2025-07-10 11:32:56 +0200

    feat: add CORS support to the application

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import 'dotenv/config';
 import express from 'express';
 import authRoutes from './src/routes/authRoutes.js';
 import { generalLimiter } from './src/middlewares/rateLimiter.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(generalLimiter);
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
